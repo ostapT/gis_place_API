49e8e2155da9f3599a8ff50a45019ea594c635c9
ostapix@gmail.com <@Bandera345>
2023-05-31 12:22:21 +0300

    Added PlaceViewSet

--- place_app/views.py
+++ place_app/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import OpenApiParameter, extend_schema
+from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from django.contrib.gis.geos import Point
+from django.contrib.gis.db.models.functions import Distance
+from .models import Place
+from .serializers import PlaceSerializer
 
-# Create your views here.
+
+class PlaceViewSet(ModelViewSet):
+    queryset = Place.objects.all()
+    serializer_class = PlaceSerializer
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "longitude",
+                OpenApiTypes.FLOAT,
+                description="Longitude, to search the nearest point",
+                required=False,
+            ),
+            OpenApiParameter(
+                "latitude",
+                OpenApiTypes.FLOAT,
+                description="Latitude, to search the nearest point",
+                required=False,
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs) -> Response:
+        longitude = request.query_params.get("longitude")
+        latitude = request.query_params.get("latitude")
+
+        if longitude and latitude:
+            point = Point(float(longitude), float(latitude), srid=4326)
+            return self.nearest(point)
+
+        return super().list(request, *args, **kwargs)
+
+    def nearest(self, point: Point):
+        nearest_place = (
+            self.queryset.annotate(distance=Distance("geom", point))
+            .order_by("distance")
+            .first()
+        )
+
+        if not nearest_place:
+            return Response(
+                {"message": "No nearest place found."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        return Response(
+            self.get_serializer(nearest_place).data, status=status.HTTP_200_OK
+        )
