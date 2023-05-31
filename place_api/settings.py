00dcb8ae1153e329fc1b7efbf57b20d193f3b83d
ostapix@gmail.com <@Bandera345>
2023-05-31 14:41:12 +0300

    Put Django Secret Key to environment variables

--- place_api/settings.py
+++ place_api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i_lc#t0ge_6i!9@=a=l=#!cfd2bvgo^vbh7#u4+xbydg$xw_ql"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
